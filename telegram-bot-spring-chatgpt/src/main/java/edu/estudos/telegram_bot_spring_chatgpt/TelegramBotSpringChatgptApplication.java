e55ca317ebe8fc37fdb48991c9b580667db9fa35
thibastos0 <thibastos@live.com>
2026-01-17 16:40:12 -0300

    inicio projeto telegrambot pom.xml e application.properties ok

---
+++ telegram-bot-spring-chatgpt/src/main/java/edu/estudos/telegram_bot_spring_chatgpt/TelegramBotSpringChatgptApplication.java
@@ -0,0 +1,13 @@
+package edu.estudos.telegram_bot_spring_chatgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotSpringChatgptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotSpringChatgptApplication.class, args);
+	}
+
+}
---
+++ telegram-bot-spring-chatgpt/src/test/java/edu/estudos/telegram_bot_spring_chatgpt/TelegramBotSpringChatgptApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.estudos.telegram_bot_spring_chatgpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotSpringChatgptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
